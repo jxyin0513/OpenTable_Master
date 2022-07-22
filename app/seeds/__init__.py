dd96b40aa79344dd40b154761e4d9a70d280313f
Luis <sans.boba@gmail.com>
2022-07-22 10:21:10 -0700

    reservation type changes revised/seeds working

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -1,5 +1,6 @@
 from flask.cli import AppGroup
 from .users import seed_users, undo_users
+from .restaurants import seed_restaurants, seed_reviews, seed_reservation, undo_re
 
 # Creates a seed group to hold our commands
 # So we can type `flask seed --help`
@@ -10,6 +11,9 @@ seed_commands = AppGroup('seed')
 @seed_commands.command('all')
 def seed():
     seed_users()
+    seed_restaurants()
+    seed_reviews()
+    seed_reservation()
     # Add other seed functions here
 
 
@@ -17,4 +21,5 @@ def seed():
 @seed_commands.command('undo')
 def undo():
     undo_users()
+    undo_re()
     # Add other undo functions here
