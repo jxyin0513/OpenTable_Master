390a6c69aefbf8b50c04790abad2134fb32d4a1c
Luis <sans.boba@gmail.com>
2022-07-26 11:54:58 -0700

    search functions and renders content

--- react-app/src/components/restaurants/SearchRestaurant.js
+++ react-app/src/components/restaurants/SearchRestaurant.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { Redirect } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import { SearchRestaurantsThunk } from '../../store/restaurant'
 import SearchResults from './SearchResults';
@@ -8,20 +8,37 @@ const Search = () => {
   const dispatch = useDispatch()
 
   const [search, setSearch] = useState('');
+  const [resultsFound, setResultsFound] = useState(false);
+  const [results, setResults] = useState([])
 
   const searchQuery = async (e) => {
     e.preventDefault();
     const res = await dispatch(SearchRestaurantsThunk(search));
-    console.log("RES:: ", res);
+    console.log("RES:: ", res.restaurants);
     if (res) {
-      return (
-        <SearchResults restaurants={res} />
-      )
+      setResultsFound(true);
+      setResults(res.restaurants)
+    } else {
+      return <h3>0 Results Found</h3>
     }
   }
+  console.log(results);
 
   return (
     <>
+      {resultsFound && (
+        <div className='results_container'>
+          <h3>{results.length} results found.</h3>
+          {results.map(result => (
+            <div key={result.id} className='results_container'>
+              <Link to={`/restaurants/${result.id}`}>{result.name}</Link>
+              <p>{result.hours}</p>
+              <p>{result.street}</p>
+              <p>{result.phone}</p>
+            </div>
+          ))}
+        </div>
+      )}
       <input type='text'
         value={search}
         onChange={e => setSearch(e.target.value)} />
