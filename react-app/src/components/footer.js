d2c5f71378576b6344fe8785d6472fb50c447e4f
allen <allenfaughn@gmail.com>
2022-07-28 17:49:59 -0500

    added a footer with minimal styling

---
+++ react-app/src/components/footer.js
@@ -0,0 +1,46 @@
+import './footer.css';
+
+const CustomFooter = () => {
+    return (
+        <div className='footer-wrapper'>
+            <div className='luis'></div>
+                <ul className='footer-links'>
+                    <li>
+                        <a href='https://github.com/stuffy-doll'><img src='/assets/images/github.png' className='gitHub' alt='gitHub' /><p>stuffy-doll</p></a>
+                    </li>
+                    <li>
+                        <a href=''><img src='/assets/images/linkedIn.png' className='linkedIn' alt='linkedIn' /><p>placeholder</p></a>
+                    </li>
+                </ul>
+            <div className='ethan'></div>
+                <ul className='footer-links'>
+                    <li>
+                        <a href='https://github.com/eebrowning'><img src='/assets/images/github.png' className='gitHub' alt='gitHub' /><p>eebrowning</p></a>
+                    </li>
+                    <li>
+                        <a href=''><img src='/assets/images/linkedIn.png' className='linkedIn' alt='linkedIn' /><p>placeholder</p></a>
+                    </li>
+                </ul>
+            <div className='allen'></div>
+                <ul className='footer-links'>
+                    <li>
+                        <a href='https://github.com/aFaughn'><img src='/assets/images/github.png' className='gitHub' alt='gitHub' /><p>aFaughn</p></a>
+                    </li>
+                    <li>
+                        <a href=''><img src='/assets/images/linkedIn.png' className='linkedIn' alt='linkedIn' /><p>placeholder</p></a>
+                    </li>
+                </ul>
+            <div className='jingxun'></div>
+                <ul className='footer-links'>
+                    <li>
+                        <a href='https://github.com/jxyin0513'><img src='/assets/images/github.png' className='gitHub' alt='gitHub' /><p>jxyin0513</p></a>
+                    </li>
+                    <li>
+                        <a href=''><img src='/assets/images/linkedIn.png' className='linkedIn' alt='linkedIn' /><p>placeholder</p></a>
+                    </li>
+                </ul>
+        </div>
+    )
+}
+
+export default CustomFooter
