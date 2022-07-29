9c7ed8453c490ba557e20ec6c6144d815a1830bc
Luis <sans.boba@gmail.com>
2022-07-29 13:39:43 -0700

    styled restaurants/login/signup v1

--- react-app/src/components/auth/LoginForm.js
+++ react-app/src/components/auth/LoginForm.js
@@ -1,12 +1,35 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { Redirect } from 'react-router-dom';
 import { login } from '../../store/session';
+import { Link } from 'react-router-dom';
+import './login-form.css'
 
 const LoginForm = () => {
   const [errors, setErrors] = useState([]);
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const greetings = [
+    'Welcome back!',
+    'So good to see you!',
+    'Hey there!',
+    'We missed you!',
+    'Hi!',
+    'Glad you\'re here!',
+    'Bonjour!',
+    'Hola!',
+    'Sup',
+    'Ready to eat?',
+    'Ready to book?',
+    'Hope you brought friends!',
+    'Hope you brought an appetite!',
+    'Yo!',
+    'Snack time!',
+    'Tables await...',
+    '😋🍕',
+    'What are ya hungry for?'
+  ]
+  const [greeting, setGreeting] = useState(greetings[Math.floor(Math.random() * greetings.length)])
   const user = useSelector(state => state.session.user);
   const dispatch = useDispatch();
 
@@ -31,34 +54,37 @@ const LoginForm = () => {
   }
 
   return (
-    <form onSubmit={onLogin}>
-      <div>
-        {errors.map((error, ind) => (
-          <div key={ind}>{error}</div>
-        ))}
-      </div>
-      <div>
-        <label htmlFor='email'>Email</label>
-        <input
-          name='email'
-          type='text'
-          placeholder='Email'
-          value={email}
-          onChange={updateEmail}
-        />
-      </div>
-      <div>
-        <label htmlFor='password'>Password</label>
-        <input
-          name='password'
-          type='password'
-          placeholder='Password'
-          value={password}
-          onChange={updatePassword}
-        />
+    <div className='login-form'>
+      <h2 className='greeting'>{greeting}</h2>
+      <h3>Log In</h3>
+      <form onSubmit={onLogin}>
+        <div className='login-errors'>
+          {errors.map((error, ind) => (
+            <div key={ind}>{error}</div>
+          ))}
+        </div>
+        <div>
+          <input
+            name='email'
+            type='text'
+            placeholder='Email'
+            value={email}
+            onChange={updateEmail}
+          />
+        </div>
+        <div>
+          <input
+            name='password'
+            type='password'
+            placeholder='Password'
+            value={password}
+            onChange={updatePassword}
+          />
+        </div>
         <button type='submit'>Login</button>
-      </div>
-    </form>
+        <p>Don't have an account? <Link to='/sign-up'>Sign up!</Link></p>
+      </form>
+    </div>
   );
 };
 
