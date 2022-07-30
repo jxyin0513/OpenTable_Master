5fc8c7f85bab1b29c77e04325926e5efc51c2760
allen <allenfaughn@gmail.com>
2022-07-30 16:50:41 -0500

    fixed users being able to set same opening and closing hours

---
+++ app/forms/custom_validators.py
@@ -0,0 +1,32 @@
+from wtforms.validators import ValidationError
+
+class NotEqualTo(object):
+    """
+    Compares the values of two fields and throws an error if they are equal.
+
+    :param fieldname:
+        The name of the other field to compare to.
+    :param message:
+        Error message to raise in case of a validation error. Can be
+        interpolated with `%(other_label)s` and `%(other_name)s` to provide a
+        more helpful error.
+    """
+    def __init__(self, fieldname, message=None):
+        self.fieldname = fieldname
+        self.message = message
+
+    def __call__(self, form, field):
+        try:
+            other = form[self.fieldname]
+        except KeyError:
+            raise ValidationError(field.gettext("Invalid field name '%s'.") % self.fieldname)
+        if field.data == other.data:
+            d = {
+                'other_label': hasattr(other, 'label') and other.label.text or self.fieldname,
+                'other_name': self.fieldname
+            }
+            message = self.message
+            if message is None:
+                message = field.gettext('Field must not be equal to %(other_name)s.')
+
+            raise ValidationError(message % d)
--- app/forms/restaurant_form.py
+++ app/forms/restaurant_form.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, IntegerField, SubmitField, TimeField, SelectField
 from wtforms.validators import DataRequired, Regexp
+from .custom_validators import NotEqualTo
 
 # Split hours field into two (Opening, Closing Times)
 CUISINE_CHOICES = ['American', 'Barbecue', 'Cafe', 'Chinese', 'Fast Food', 'Indian', 'Italian', 'Japanese', 'Korean BBQ', 'Mediterranean', 'Mexican', 'Middle Earth', 'Thai', 'Vegan']
@@ -12,7 +13,7 @@ class NewRestaurantForm(FlaskForm):
     phone = StringField('Phone', validators=[Regexp('^(\+\d{1,2}\s?)?1?\-?\.?\s?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}$', message="Please provide correct format")])
     street = StringField('Street', validators=[DataRequired(message="Please provide your street address.")])
     cuisine = StringField('Cuisine', validators=[DataRequired(message="Select your cuisine type")])
-    open_hours = TimeField('Open', validators=[DataRequired(message="Please provide your open hour")])
+    open_hours = TimeField('Open', validators=[DataRequired(message="Please provide your open hour"), NotEqualTo('close_hours', message='Open Hours may not equal Close Hours')])
     close_hours = TimeField('Close', validators=[DataRequired(message="Please provide your close hour")])
     image_url = StringField('Image')
     price_point = IntegerField('Price Point', validators=[DataRequired(message="Please provide your price point")])
